a9c44c91f530a7b2a0c1e7f76549b9852e592a19
sAfridi <shafi.afridi786@gmail.com>
2022-05-08 12:04:42 +0200

    SAFA-ECM

---
+++ code/Python/ECM/result_ECM_output.py
@@ -0,0 +1,15 @@
+import os,glob
+from time import sleep
+
+# Select all 'graphml' files in current directory
+myl=sorted(glob.glob('*graphml'))
+print (myl)
+
+#Range define the required no. of subsets of nodes
+for j in (myl):
+    for i in range(4,9,1):
+        os.system("python.exe ECM-SubsetNodes.py %s %d > %s-%d-ECM.txt"
+                  %(j,i, j.replace(".graphml", ""),i))
+
+
+
